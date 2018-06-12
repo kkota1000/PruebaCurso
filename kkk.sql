11868227a9f71dd1e06dd293e1e58b758cc44746
kkota1000 <40201896+kkota1000@users.noreply.github.com>
2018-06-12 13:11:42 +0200

    Add files via upload

---
+++ kkk.sql
@@ -0,0 +1,18 @@
+Create or replace procedure name(param in param_type) is
+
+CURSOR CKK (pkk kkk.kkkl%type)  IS
+  SELECT * FROM KKK WHERE SDR = pkk;
+
+
+CURSOR cKK2 (pkk kkk.kkkl%type)  IS
+  SELECT * FROM KKK WHERE SDR = pkk;
+
+begin
+  
+  open CKK
+  fetch CKK into vkk;
+  whi
+
+end;
+
+end name;
